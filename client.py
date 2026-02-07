72dce4fe93e5da801007a71de4ff8b9375538efc
NewMicRob <microbnew@gmail.com>
2026-02-06 21:14:29 -0500

    4B - Lab Completed
    Paragraph will be in the README as well as submitted by word doc.

--- client.py
+++ client.py
@@ -3,15 +3,19 @@
 import socket
 import argparse
 import struct
+import time
 import os
 
 def run_client(target_ip, target_port, input_file):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_address = (target_ip, target_port)
-    sock.settimeout(0.5)
+    sock.settimeout(0.1)
     print(f"[*] Sending file '{input_file}' to {target_ip}:{target_port}")
 
     seq_num = 0
+    buffer = {}
+    retrans_time = 0.5
+    eof_received = False
 
     if not os.path.exists(input_file):
         print(f"[!] Error: File '{input_file}' not found.")
@@ -22,28 +26,50 @@ def run_client(target_ip, target_port, input_file):
     try:
         with open(input_file, 'rb') as f:
             while True:
-                chunk = f.read(4092)
                 
-                if not chunk:
-                    break
-                
-                header = struct.pack('!I', seq_num)
-                packet = header + chunk
-
+                if not eof_received and (seq_num - sum(1 for i in buffer.values() if i['acked']) < 50):                    
+                    chunk = f.read(4092)
+                    
+                    if chunk:
+                        header = struct.pack('!I', seq_num)
+                        packet = header + chunk
+                        buffer[seq_num] = {
+                            'data': packet,
+                            'time': time.time(),
+                            'acked': False
+                        }
+                        sock.sendto(packet, server_address)
+                        seq_num += 1
+                    else:
+                        eof_received = True
+                    
                 while True: 
-                    sock.sendto(packet, server_address)
+                    
                     try:
                         ack_packet, _ = sock.recvfrom(4)
                         ack_num = struct.unpack('!I', ack_packet)[0]
-                        if ack_num == seq_num:
-                            seq_num += 1
-                            break
+                        if ack_num in buffer:
+                            buffer[ack_num]['acked'] = True
                     except socket.timeout:
-                        continue
-
-        sock.sendto(b'', server_address)
+                        break
+                    
+                now = time.time()
+                
+                for seq_id, into in buffer.items():
+                    
+                    if not into['acked'] and now - into['time'] > retrans_time:
+                        sock.sendto(into['data'], server_address)
+                        buffer[seq_id]['time'] = now
+                        
+                if eof_received and all(info['acked'] for info in buffer.values()):
+                    break
+                    
+                time.sleep(0.001)
+                            
+        eof_header = struct.pack('!I', seq_num)
+        sock.sendto(eof_header + b'', server_address)
         print("[*] File transmission complete.")
-
+                    
     except Exception as e:
         print(f"[!] Error: {e}")
     finally:
--- server.py
+++ server.py
@@ -12,35 +12,49 @@ def run_server(port, output_file):
     sock.bind(server_address)
     
     exp_num = 0
+    buffer = {}
+    eof_seq = None
     f = None
+    
     try:
         while True:
             packet, addr = sock.recvfrom(4096)
             if not packet:
+                continue
+            
+            seq_num = struct.unpack('!I', packet[:4])[0]
+            data = packet[4:]
+            ack = struct.pack('!I', seq_num)
+            sock.sendto(ack, addr)
+
+            if not data:
+                eof_seq = seq_num
+            else:
+                if f is None:
+                    print("==== Start of reception ====")
+                    ip, sender_port = addr
+                    sender_filename = f"received_{ip.replace('.', '_')}_{sender_port}.jpg"
+                    f = open(sender_filename, 'wb')
+                    print(f"[*] First packet received from {addr}. File opened for writing as '{sender_filename}'.")
                 
+                if seq_num == exp_num:
+                    f.write(data)
+                    exp_num += 1
+                
+                    while exp_num in buffer:
+                        f.write(buffer.pop(exp_num))
+                        exp_num += 1
+                elif seq_num > exp_num:
+                    buffer[seq_num] = data
+            
+            if eof_seq is not None and exp_num == eof_seq and not buffer:
                 if f:
                     f.close()
                     f = None
                     print(f"[*] End of file signal received from {addr}. File closed.")
                 exp_num = 0
-                continue
-                
-            seq_num = struct.unpack('!I', packet[:4])[0]
-            data = packet[4:]
-
-            if f is None:
-                print("==== Start of reception ====")
-                ip, sender_port = addr
-                sender_filename = f"received_{ip.replace('.', '_')}_{sender_port}.jpg"
-                f = open(sender_filename, 'wb')
-                print(f"[*] First packet received from {addr}. File opened for writing as '{sender_filename}'.")
-            
-            if seq_num == exp_num:
-                f.write(data)
-                exp_num += 1
-                
-            ack= struct.pack('!I', seq_num)
-            sock.sendto(ack, addr)
+                buffer.clear()
+                eof_seq = None
 
     except KeyboardInterrupt:
         print("==== End of reception ====")
